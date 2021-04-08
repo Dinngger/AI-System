bc779521d6a5cd8c58e25c2fe51feae7f75e0e6d
Dinngger <814089338@qq.com>
2021-04-08 10:22:40 +0800

    add lab 7

---
+++ Labs/AdvancedLabs/Lab7/plot.py
@@ -0,0 +1,4 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.asarray([8, 16, 32, 64])
--- Labs/AdvancedLabs/Lab7/pytorch_mnist_horovod.py
+++ Labs/AdvancedLabs/Lab7/pytorch_mnist_horovod.py
@@ -29,7 +29,7 @@ parser.add_argument('--batch-size', type=int, default=64, metavar='N',
                     help='input batch size for training (default: 64)')
 parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
                     help='input batch size for testing (default: 1000)')
-parser.add_argument('--epochs', type=int, default=10, metavar='N',
+parser.add_argument('--epochs', type=int, default=1, metavar='N',
                     help='number of epochs to train (default: 10)')
 parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
                     help='learning rate (default: 0.01)')
@@ -146,7 +146,7 @@ if __name__ == '__main__':
         kwargs['multiprocessing_context'] = 'forkserver'
 
     train_dataset = \
-        datasets.MNIST('data-%d' % hvd.rank(), train=True, download=True,
+        datasets.MNIST('data', train=True, download=False,
                        transform=transforms.Compose([
                            transforms.ToTensor(),
                            transforms.Normalize((0.1307,), (0.3081,))
@@ -158,7 +158,7 @@ if __name__ == '__main__':
         train_dataset, batch_size=args.batch_size, sampler=train_sampler, **kwargs)
 
     test_dataset = \
-        datasets.MNIST('data-%d' % hvd.rank(), train=False, transform=transforms.Compose([
+        datasets.MNIST('data', train=False, transform=transforms.Compose([
             transforms.ToTensor(),
             transforms.Normalize((0.1307,), (0.3081,))
         ]))
